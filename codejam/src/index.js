5372ea0bacc04d0c23ee08a90aeff676cf2ee4ca
Dmitry Ivanov <dimannisik1@gmail.com>
2022-10-25 03:48:16 +0700

    feat: add icon

--- codejam/src/index.js
+++ codejam/src/index.js
@@ -8,6 +8,7 @@ import { resizeWindow } from "./listeners/resizeWindow";
 import { clickCurtainMenu } from "./listeners/clickCurtainMenu";
 import checkWin from "./helpers/checkWin";
 import { hoverButtonsSoundPlay } from "./listeners/hoverButtonsSoundPlay";
+import icon from "./assets/favicon.png";
 
 let size = 4;
 let matrix = getSolvableMatrix(size);
@@ -35,4 +36,18 @@ window.addEventListener(
     listener.abort();
   },
   listener
+);
+
+(function () {
+  var link =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement("link");
+  link.type = "image/x-icon";
+  link.rel = "shortcut icon";
+  link.href = icon;
+  document.getElementsByTagName("head")[0].appendChild(link);
+})();
+
+alert(
+  "Привет, остались небольшие недочеты, мне нужно еще немного времени. Пожалуйста, проверь мою работу ближе к концу дедлайна. Спасибо за понимание и хорошего дня."
 );
